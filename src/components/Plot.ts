d0f29606c0f66a4a8ab79304e3a0c1512e8b20d4
Jules Fouchy <jules.fouchy@ntymail.com>
2020-11-30 14:19:20 +0100

    Show error messages when typing in invalid function expression

--- src/components/Plot.ts
+++ src/components/Plot.ts
@@ -5,8 +5,13 @@ import type { Func } from '../types/Func'
 window.d3 = require("d3")
 const functionPlot = require("function-plot")
 
+const setPlotErrorMessage = (msg: string) => {
+    document.getElementById('plot-error').innerHTML = msg
+}
+
 const Input = (func: Func) => h('input', {
     oninput: (state: State, event) => {
+        setPlotErrorMessage('')
         const stateCopy: State = {...state}
         stateCopy.functions[func.index].value = event.target.value
         return stateCopy
@@ -16,24 +21,40 @@ const Input = (func: Func) => h('input', {
 
 const Plot = (myFunc: Func, targetFunc: Func) => {
     const id = 'main-function-plot'
-    setTimeout(() =>
-        functionPlot({
-            target: '#'+id,
-            width: 400,
-            height: 400,
-            disableZoom: true,
-            grid: true,
-            data: [
-                { fn: myFunc.value },
-                { fn: targetFunc.value },
-            ]
-        }),
-    0)
+    setTimeout(() => {
+        try {
+            functionPlot({
+                target: '#'+id,
+                width: 400,
+                height: 400,
+                disableZoom: true,
+                grid: true,
+                data: [
+                    { fn: myFunc.value },
+                    { fn: targetFunc.value },
+                ]
+            })
+        }
+        catch(e) {
+            setPlotErrorMessage(e)
+        }
+    }
+    , 0)
 
     return h('div', {id, class: 'functionPlot'}, [])
 }
 
-export default (myFunc: Func, targetFunc: Func) => h('div', {}, [
-    Plot (myFunc, targetFunc),
-    Input(myFunc),
-])
+export default (myFunc: Func, targetFunc: Func) => {
+    return h(
+        'div', {}, 
+        [
+            Plot (myFunc, targetFunc),
+            Input(myFunc),
+            h('div', 
+            {
+                id: 'plot-error'
+            }
+            ),
+        ]
+    )
+}
